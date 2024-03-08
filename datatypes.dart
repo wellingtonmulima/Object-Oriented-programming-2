1c927529d1a11031c9744845230694cdbcbf13f6
codedark1 <wellingtonmulima@gmail.com>
2024-03-08 13:07:11 +0300

    first commit

---
+++ datatypes.dart
@@ -0,0 +1,21 @@
+void main(){
+  String name='Wellington Mulima'; //string used to store data in the program
+  int age=27;// used to store whole numbers in the program
+  double pie=3.142;//stores decimal numbers
+
+  List<String> f_names=['wellington','precious','julian','dennis','kevin'];//stores multiple values without creating multiple variables
+
+
+
+  Map<String,int> marks={'Eladams':10,'richard':7,'bonventure':23,'esther':25};
+
+  print('Your name is: $name');//prints name
+  print('Your age is: $age');//prints age
+  print('The value for pie is $pie');//prints value of pie
+
+  print('value of first names is $f_names');//prints names on the list
+  print('value of first names[0] is ${f_names[0]}');//index 0
+  print('marks of students:$marks');//prints name and age 
+  print('$marks[0]');
+
+}
